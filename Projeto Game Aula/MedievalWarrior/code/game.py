aec947e94072506064c21ffe68f76c564f10b6f2
MagaDragunn <hugo_magaa@outlook.com>
2025-06-16 20:54:17 -0300

    creating main loop Creating connecting menu end game classes

--- Projeto Game Aula/MedievalWarrior/code/game.py
+++ Projeto Game Aula/MedievalWarrior/code/game.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame as py
+from code.menu import Menu
 
 class Game:
     def __init__(self):
         self.window = None
+        py.init()
+        py.display.set_mode(size=(800, 600))
+    def run(self):
+        while True:
+            menu = Menu(self.window)
+            menu.run()
+            pass
 
-    def run(self, ):
-        pass
+
+
+            # Check for all events
+            #for event in py.event.get():
+            #   if event.type == py.QUIT:
+            #       py.quit()  # close Windows
+            #       quit()  # end pygame
