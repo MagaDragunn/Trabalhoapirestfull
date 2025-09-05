b2c5a3bcfb7e457aba3897db3c28bacda96537e7
MagaDragunn <hugo_magaa@outlook.com>
2025-09-05 10:52:21 -0300

    Delete demo directory

--- demo/src/main/java/com/example/demo/daoo/TarefaDaoImpl.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.daoo;
-
-import org.springframework.stereotype.Repository;
-
-import com.example.demo.entity.Tarefa;
-@Repository
-public class TarefaDaoImpl extends AbstractDao<Tarefa, Long> implements TarefaDao  {
-
-}
